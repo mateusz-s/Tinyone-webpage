0a82ebccace12924d50f77ead28da02fceeceee2
Mateusz Smykowski <msmykowski@interia.pl>
2017-10-05 22:34:52 +0200

    Add smooth scrolling script + AOS init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+AOS.init({
+	duration: 700,
+	easing: 'ease-in'
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('**html, body**').animate({ // Bugfix jQuery animate scrolling for Mozilla Firefox
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
